65e887cb04fff41c6a9e97c21d1bab9e3f819bbf
LucieRC <LucieRC@users.noreply.github.com>
2023-12-10 08:38:20 +0000

    deploy: 26e4358c5517bd247e7276afafb83bf5cc4b0384

---
+++ navtreedata.js
@@ -0,0 +1,107 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Antares Simulator", "index.html", [
+    [ "How does the automatic testing script system work ?", "md_src_tests_run_study_tests_readme.html", [
+      [ "Introduction", "md_src_tests_run_study_tests_readme.html#autotoc_md1", null ],
+      [ "Entry points to the automatic testing script system", "md_src_tests_run_study_tests_readme.html#autotoc_md2", null ],
+      [ "File test_from_json.py", "md_src_tests_run_study_tests_readme.html#autotoc_md3", null ],
+      [ "Fixtures", "md_src_tests_run_study_tests_readme.html#autotoc_md4", null ],
+      [ "Tests parametrization", "md_src_tests_run_study_tests_readme.html#autotoc_md5", null ],
+      [ "The test's body", "md_src_tests_run_study_tests_readme.html#autotoc_md6", [
+        [ "<ins>Run fixtures</ins>", "md_src_tests_run_study_tests_readme.html#autotoc_md7", null ],
+        [ "<ins>Checks creation factory</ins>", "md_src_tests_run_study_tests_readme.html#autotoc_md8", null ],
+        [ "<ins>Running the checks</ins>", "md_src_tests_run_study_tests_readme.html#autotoc_md9", null ],
+        [ "<ins>End of tests</ins>", "md_src_tests_run_study_tests_readme.html#autotoc_md10", null ]
+      ] ],
+      [ "How to add a check on a test ?", "md_src_tests_run_study_tests_readme.html#autotoc_md11", [
+        [ "What to do ?", "md_src_tests_run_study_tests_readme.html#autotoc_md12", null ],
+        [ "What NOT to do ?", "md_src_tests_run_study_tests_readme.html#autotoc_md13", null ]
+      ] ],
+      [ "Running the script", "md_src_tests_run_study_tests_readme.html#autotoc_md14", null ],
+      [ "TO DO", "md_src_tests_run_study_tests_readme.html#autotoc_md15", null ]
+    ] ],
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", "functions_type" ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"AreaBalance_8h_source.html",
+"classAntares_1_1Action_1_1AntaresStudy_1_1Area_1_1NodeTimeseries.html#a554e1900543138faaaecc289ec759ab6",
+"classAntares_1_1CBuilder.html#ad978d16efa6f14e71b1b81cb41d51daf",
+"classAntares_1_1Component_1_1Datagrid_1_1Renderer_1_1ConstraintsBuilder_1_1Links_1_1Record.html#a5a506e80e0cbdc51afb8dd7988dda13a",
+"classAntares_1_1Component_1_1Datagrid_1_1Renderer_1_1Misc.html#ac1f73f29c2481db5d76dae0c82540523",
+"classAntares_1_1Component_1_1Datagrid_1_1Renderer_1_1connectionNTCindirect.html",
+"classAntares_1_1Component_1_1Notebook_1_1Page.html#a30481e8f7722ad14f2039d24592ac055",
+"classAntares_1_1Data_1_1BindingConstraint.html#a7a34d9a77804194550b53a75729fc1a5",
+"classAntares_1_1Data_1_1Parameters.html#a58d06d92abdda6053d20b5627309ad62",
+"classAntares_1_1Data_1_1Sets.html#ad4780d1b6c185211bd373b48e5c42628",
+"classAntares_1_1Data_1_1ThermalCluster.html#a8af7d95b5e9805d840d4c9b091073910",
+"classAntares_1_1Graph_1_1Grid.html#ab24958f2e08e120a2e6f869be8f3b242",
+"classAntares_1_1Map_1_1Tool_1_1Tool.html#a05b0b333bdc2dd0079a0913edf461d50",
+"classAntares_1_1Solver_1_1Simulation_1_1BindingConstraintsTimeSeriesNumbersWriter.html",
+"classAntares_1_1Solver_1_1Variable_1_1Economy_1_1OverallCost.html#ab6693b6e18ec12db223458bb0328953a",
+"classAntares_1_1Solver_1_1Variable_1_1State.html#a92081f2c3063cbdecb7b19dc4023827f",
+"classAntares_1_1Toolbox_1_1Process_1_1JobExecute.html#a14d4a2bee83afdada0de6e59de8a847b",
+"classAntares_1_1Window_1_1OutputViewer_1_1SpotlightProvider.html#a6cbd655ecab658898491b9c3dcc97ff6",
+"classIProvider.html#a5b16afcad86e27bb0f50457654a16ac7",
+"context_8h_source.html",
+"dir_efd047713bcd1ccefe55d92ae4bcf67b.html",
+"libs_2antares_2array_2antares_2array_2matrix_8hxx_source.html",
+"saveas_8hxx_source.html",
+"structAntares_1_1Solver_1_1Variable_1_1Economy_1_1STstorageWithdrawalByCluster_1_1Statistics.html",
+"structAntares_1_1Window_1_1Inspector_1_1PAdequacyPatchMode.html",
+"updater_8hxx_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
